11b80fd8e087921e33ac8b220d227903efd04fc7
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-10-20 17:44:34 -0600

    fixed supabase loop in header

--- src/js/header.mjs
+++ src/js/header.mjs
@@ -73,5 +73,11 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.error("Error checking Supabase session:", error);
     }
     // live update header with auth changes
-    // supabase.auth.onAuthStateChange(() => location.reload());
+    supabase.auth.onAuthStateChange((event) => {
+        console.log("auth event:", event);
+        if (event === "SIGNED_IN" || event === "SIGNED_OUT") {
+            // reload only once when logged in or out
+            location.reload();
+        } 
+    });
 });
